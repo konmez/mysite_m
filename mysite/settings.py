699bd2827883f38d88e603370ac798709213df35
konmez <kmezin@yahoo.com>
2025-03-03 07:21:53 +0000

    set DB for deployment

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,16 +139,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(DATABASE_URL)
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(DATABASE_URL)
+}
 
 
 
@@ -257,7 +257,8 @@ else:
 
 
  
-LOGIN_REDIRECT_URL = 'account:dashboard'   
+LOGIN_REDIRECT_URL = 'account:dashboard'    
+
 LOGOUT_REDIRECT_URL = 'account:login'
 LOGOUT_URL = 'account:logout'  # or 'dashboard'
 LOGIN_URL = 'account:login'  # or 'account_login' or 'login' or 'account_login`
